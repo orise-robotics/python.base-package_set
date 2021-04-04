5454cb15192bcc5d6a3232785cceb6a165b87b7a
ivan1993br <ivan1993br@gmail.com>
2021-04-04 00:57:41 -0300

    Add python setup-tools tests

--- autobuild/python_setuptools.rb
+++ autobuild/python_setuptools.rb
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+require 'autobuild/configurable'
+
 module Autobuild
   def self.python_setuptools(options, &block)
       PythonSetuptools.new(options, &block)
@@ -20,12 +22,27 @@ module Autobuild
   class PythonSetuptools < Autobuild::Python
       def initialize(options)
           super(options)
+          @python_bin = Autoproj.workspace.config.get('python_executable', nil)
+          setup_tests
           @mutex = Mutex.new
       end
 
       def generate_install_data_command
-        command = ['python', 'setup.py', 'install_data']
+        command = [@python_bin, 'setup.py', 'install_data']
         command << "--install-dir=#{prefix}"
+        command
+      end
+
+      def generate_tests_data_command(test_file)
+        test_utility.source_ref_dir = builddir
+        command = [@python_bin , '-m', 'pytest']
+        command << "-o"
+        command << "cache_dir=#{@builddir}"
+        command << "--tb=short"
+        test_name = File.basename(test_file, ".*")
+        testsout_filename = File.join(@builddir, 'test_results', @name, "#{test_name}.xml")
+        command << "--junit-xml=#{testsout_filename}"
+        command << test_file
         command
       end
 
@@ -60,6 +77,40 @@ module Autobuild
       def install
         super
         install_data
+        Autoproj.env_add_path 'AMENT_PREFIX_PATH', @prefix
+      end
+
+      def setup_tests
+        unless test_utility.has_task?
+          test_dir = File.join(srcdir, 'test')
+          if File.directory?(test_dir)
+            test_utility.source_dir = File.join('test_results', name)
+          end
+  
+          with_tests if test_dir
+        end
+      end
+
+      # Copied from cmake to use doc and tests
+      def common_utility_handling(utility, target, start_msg, done_msg)
+        utility.task do
+            progress_start start_msg, :done_message => done_msg do
+                all_files = Dir["#{srcdir}/test/**/test_*.py"]
+                all_files.each do |test_file|
+                    run(utility.name,
+                    *generate_tests_data_command(test_file),
+                    working_directory: srcdir)
+                end
+                yield if block_given?
+            end
+        end
+      end
+
+      def with_tests(target = 'test', &block)
+        common_utility_handling(
+            test_utility, target,
+            "running tests for %s",
+            "successfully ran tests for %s", &block)
       end
 
       def import(options)
